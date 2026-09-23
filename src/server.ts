7bab5966b454c778031387aea27b36f5b4600969
shantpr05 <shanthi.parvatham@hyperisland.se>
2026-09-23 10:50:42 +0200

    implemented get all transactions

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express, { type Request, type Response } from "express";
+import transactions from "../data/transactions.json";
 const app = express();
 const PORT = 3000;
 
 // Middleware
 app.use(express.json());
+
+app.get("/transactions", (req: Request, res: Response) => {
+  res.status(200).json(transactions);
+});
+
 
 // Start the server on port 3000
 app.listen(PORT, () => {
