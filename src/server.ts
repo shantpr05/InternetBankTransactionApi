4971fde1eea4c1c8e5c98d79af01284c99438064
Dantewar <warda@live.se>
2026-09-23 11:39:00 +0200

    feat: implement GET classifications

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
 import express, { type Request, type Response } from "express";
 import transactions from "../data/transactions.json";
+import classifications from "../data/classifications.json";
+
 const app = express();
 const PORT = 3000;
 
 // Middleware
 app.use(express.json());
 
+// GET all transactions
 app.get("/transactions", (req: Request, res: Response) => {
   res.status(200).json(transactions);
 });
 
+// GET all classifications
+app.get("/classifications", (req: Request, res: Response) => {
+  res.status(200).json(classifications);
+});
 
 // Start the server on port 3000
 app.listen(PORT, () => {
